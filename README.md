db1c7fc755651eefb6a5342a54b518f3f4369430
Omar Abdelkafi <omar.abdelkafi@tekab.dev>
2023-08-09 12:02:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Prisma + Cloudflare Workers
+
+Deployed app: [https://prisma-cf-workers.ruheni.workers.dev](https://prisma-cf-workers.ruheni.workers.dev)
+
+Endpoints:
+
+## `GET`
+- `/quotes`- return all quotes
+-  `/quotes/:id`– return a quote by id
+## `POST`
+
+- POST `/quotes` - create a new quote
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client/edge"
+import { Router } from "itty-router"
+
+const router = Router()
+const prisma = new PrismaClient()
+
+const headers = {
+  "Content-Type": "application/json"
+}
+
+router.get("/quotes", async () => {
+  const results = await prisma.quote.findMany()
+  return new Response(JSON.stringify(results, null, 2), {
+    headers
+  })
+})
+
+router.get("/quotes/:id", async ({ params }) => {
+  // @ts-ignore
+  const { id } = params
+
+  const result = await prisma.quote.findUnique({
+    where: {
+      id
+    }
+  })
+
+  return new Response(JSON.stringify(result), {
+    headers
+  })
+})
+
+router.post("/quotes", async (request: Request) => {
+  // @ts-ignore
+  const { content, author } = await request.json()
+
+  const result = await prisma.quote.create({
+    data: {
+      content,
+      author
+    }
+  })
+
+  return new Response(JSON.stringify(result, null, 2), {
+    headers
+  })
+})
+
+/** handle 404 results */
+router.all('*', () => new Response('Not Found.', { status: 404 }))
+
+addEventListener('fetch', event =>
+  event.respondWith(router.handle(event.request))
+)
